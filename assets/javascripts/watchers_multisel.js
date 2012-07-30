8256a681a5c5a32897badfb71e733119d8c02db3
jbbarth <jeanbaptiste.barth@gmail.com>
2012-07-30 17:36:48 +0200

    Formatting (JS)

--- assets/javascripts/watchers_multisel.js
+++ assets/javascripts/watchers_multisel.js
@@ -1,39 +1,35 @@
-function checkAll(name)
-{
-    boxes=document.getElementsByName(name)
-    for (i = 0; i < boxes.length; i++)
-	boxes[i].checked = true ;
-    return false;
+function checkAll(name) {
+  boxes = document.getElementsByName(name)
+  for (i = 0; i < boxes.length; i++)
+    boxes[i].checked = true;
+  return false;
 }
 
-function uncheckAll(name)
-{
-    boxes=document.getElementsByName(name)
-    for (i = 0; i < boxes.length; i++)
-	boxes[i].checked = false ;
-    return false;
+function uncheckAll(name) {
+  boxes = document.getElementsByName(name)
+  for (i = 0; i < boxes.length; i++)
+    boxes[i].checked = false;
+  return false;
 }
 
-function checkSome(name)
-{
-    var selected_values=document.getElementById('watcher_multiple_role').options[document.getElementById('watcher_multiple_role').selectedIndex].value.split(',');
-    boxes=document.getElementsByName(name)
-    for (i = 0; i < boxes.length; i++){
-        for (j = 0; j < selected_values.length; j++){
-            if(boxes[i].value == selected_values[j]) boxes[i].checked = true ;
-        }
-        }
-    return false;
+function checkSome(name) {
+  var selected_values=document.getElementById('watcher_multiple_role').options[document.getElementById('watcher_multiple_role').selectedIndex].value.split(',');
+  boxes = document.getElementsByName(name)
+  for (i = 0; i < boxes.length; i++) {
+    for (j = 0; j < selected_values.length; j++) {
+      if(boxes[i].value == selected_values[j]) boxes[i].checked = true;
+    }
+  }
+  return false;
 }
 
-function uncheckSome(name)
-{
-    var selected_values=document.getElementById('watcher_multiple_role').options[document.getElementById('watcher_multiple_role').selectedIndex].value.split(',');
-    boxes=document.getElementsByName(name)
-    for (i = 0; i < boxes.length; i++){
-        for (j = 0; j < selected_values.length; j++){
-            if(boxes[i].value == selected_values[j]) boxes[i].checked = false ;
-        }
-        }
-    return false;
+function uncheckSome(name) {
+  var selected_values = document.getElementById('watcher_multiple_role').options[document.getElementById('watcher_multiple_role').selectedIndex].value.split(',');
+  boxes = document.getElementsByName(name)
+  for (i = 0; i < boxes.length; i++) {
+    for (j = 0; j < selected_values.length; j++) {
+      if(boxes[i].value == selected_values[j]) boxes[i].checked = false;
+    }
+  }
+  return false;
 }
