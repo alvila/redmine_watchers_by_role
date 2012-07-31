c1b9152ed5b5a0264e0f8fec52387d9e203db21f
jbbarth <jeanbaptiste.barth@gmail.com>
2012-07-31 08:58:05 +0200

    Rewrite part of the javascript with Prototype (some calls were not working cross-browser)

--- assets/javascripts/watchers_multisel.js
+++ assets/javascripts/watchers_multisel.js
@@ -1,20 +1,31 @@
+//WARNING: Prototype inside!
+//TODO: move it to Jquery when Redmine migrates...
+
+//Replacement for document.getElementsByName
+//see: https://prototype.lighthouseapp.com/projects/8886/tickets/637-documentgetelementsbyname-method
+function $N(element) {
+  if (typeof element == 'string')
+    element = document.getElementsByName(element);
+  return Element.extend(element);
+}
+
 function checkAll(name) {
-  boxes = document.getElementsByName(name)
+  boxes = $N(name)
   for (i = 0; i < boxes.length; i++)
     boxes[i].checked = true;
   return false;
 }
 
 function uncheckAll(name) {
-  boxes = document.getElementsByName(name)
+  boxes = $N(name)
   for (i = 0; i < boxes.length; i++)
     boxes[i].checked = false;
   return false;
 }
 
 function checkSome(name) {
-  var selected_values=document.getElementById('watcher_multiple_role').options[document.getElementById('watcher_multiple_role').selectedIndex].value.split(',');
-  boxes = document.getElementsByName(name)
+  var selected_values=$('watcher_multiple_role').options[$('watcher_multiple_role').selectedIndex].value.split(',');
+  boxes = $N(name)
   for (i = 0; i < boxes.length; i++) {
     for (j = 0; j < selected_values.length; j++) {
       if(boxes[i].value == selected_values[j]) boxes[i].checked = true;
@@ -24,8 +35,8 @@ function checkSome(name) {
 }
 
 function uncheckSome(name) {
-  var selected_values = document.getElementById('watcher_multiple_role').options[document.getElementById('watcher_multiple_role').selectedIndex].value.split(',');
-  boxes = document.getElementsByName(name)
+  var selected_values = $('watcher_multiple_role').options[$('watcher_multiple_role').selectedIndex].value.split(',');
+  boxes = $N(name)
   for (i = 0; i < boxes.length; i++) {
     for (j = 0; j < selected_values.length; j++) {
       if(boxes[i].value == selected_values[j]) boxes[i].checked = false;
