37978d70e4305d8fb90f8debe6d04ee74564bb8a
jbbarth <jeanbaptiste.barth@gmail.com>
2012-07-31 08:57:39 +0200

    Fixed custom js not included because of the way the view hook was declared (the view was not processed in the correct context, hence content_tags not working correctly)

--- lib/redmine_watchers_by_role/hooks.rb
+++ lib/redmine_watchers_by_role/hooks.rb
@@ -1,15 +1,5 @@
 module RedmineWatchersByRole
   class Hooks < Redmine::Hook::ViewListener
-    def view_issues_form_details_bottom(context={ })
-      controller = context[:controller]
-      return '' unless controller
-      action_name = controller.action_name
-      return '' unless action_name
-      return '' unless action_name=="new"
-      context[:controller].send(:render_to_string, {
-          :partial => 'watchers/multiselect_role',
-          :locals => context
-        })
-    end
+    render_on :view_issues_form_details_bottom, :partial => 'watchers/multiselect_role'
   end
 end
