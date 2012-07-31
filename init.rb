8eda9b0e8eaa9c18dd31698355a8444071dc939b
jbbarth <jeanbaptiste.barth@gmail.com>
2012-07-31 07:03:47 +0200

    Removed useless permission 'display_in_selection' (doesn't matter if there are more roles in the select does it ?)

--- init.rb
+++ init.rb
@@ -7,8 +7,5 @@ Redmine::Plugin.register :redmine_watchers_by_role do
   description 'Allows issue creator to select multiple watchers by role assigment'
   version '0.0.1'
   author_url 'http://www.alvila.com'
-  project_module "multiple_watchers" do
-    permission :display_in_selection, { :watchers => :permissions }
-  end
 end
 
